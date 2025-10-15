acb947f06e3781ed0bea875070e3a946980d29b7
loverboy787877 <93593646+loverboy787877@users.noreply.github.com>
2025-10-15 13:06:07 +0800

    My Vue Task App

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+const newTask = ref("");
+const error = ref("");
+const emit = defineEmits <{
+    addTask: [newTask: string]
+}>();
+function formSubmitted(){
+    if (newTask.value.trim()){
+    emit("addTask", newTask.value.trim());
+    newTask.value = "";
+    }
+    else {
+        error.value = "Task cannot be empty!";
+    }
+
+}
+
+</script>
+
+
+<template>
+    <form @submit.prevent="formSubmitted">
+    <label>
+      New Task
+      <input 
+      v-model="newTask" 
+      name="newTask" 
+      :aria-invalid="!! error || undefined" 
+      @input ="error = ''"
+      >
+      <small v-if="error" id="invalid-helper">
+        {{ error }}
+      </small>
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import type {Task} from '../types';
+
+
+const props = defineProps<{
+    tasks: Task[]
+}>();
+
+const emits = defineEmits<{
+    toggleDone: [id: string]
+    removeTask: [id: string]
+}>();
+
+</script>
+
+
+
+<template>
+        <TransitionGroup name="list" tag="div" class="task-list">
+            <article v-for="task in props.tasks" class="task" :key="task.id">
+                <label>
+                    <input @input="emits('toggleDone', task.id)" 
+                    :checked="task.done" 
+                    type="checkbox"
+                    >
+                    <span :class="{done: task.done}">{{ task.title }}</span>
+                </label>
+                <button @click="emits('removeTask', task.id)" class="outline">Remove</button>
+            </article>
+    </TransitionGroup>
+</template>
+
+<style>
+
+.task-list {
+    margin-top: 1rem;
+}
+.done {
+    text-decoration: line-through;
+}
+.task {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.list-enter-active,
+.list-leave-active {
+    transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(300px);
+}
+</style>
